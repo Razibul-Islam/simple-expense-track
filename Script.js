79a480078872af75c497eaad9a99e0ba46dd2ae8
Razibul-Islam <razibulislam665@gmail.com>
2025-02-21 11:12:13 +0600

    update javascript

--- Script.js
+++ Script.js
@@ -9,52 +9,64 @@ let List = document.getElementById("list");
 // Form manopulation
 // Diposit
 let datas = [];
+
+// UI Update
+function updateUI() {
+  List.innerHTML = "";
+  datas.map((data) => {
+    let li = document.createElement("li");
+    li.classList.add(data.type === "Diposit" ? "plus" : "minus");
+    li.innerHTML = `${data.TextValue}<span>${data.type}</span><span>$${data.AmountValue}</span>`;
+    List.appendChild(li);
+  });
+}
+
 function Diposit() {
   event.preventDefault();
   let TextValue = Text.value;
   let AmountValue = Amount.value;
+
   if (parseInt(AmountValue) < 100) {
     alert("Minimum value is 100");
-  } else {
-    datas.push({ TextValue, AmountValue, Diposit: "Diposit" });
-    let DepositValue = Deposit.innerText;
-    let BalanceValue = Balance.innerText;
-    let newDepositValue = parseInt(DepositValue) + parseInt(AmountValue);
-    let newBalanceValue = parseInt(BalanceValue) + parseInt(AmountValue);
-    Deposit.innerText = newDepositValue;
-    Balance.innerText = newBalanceValue;
-    List.innerHTML = "";
-    datas.map((data) => {
-      let li = document.createElement("li");
-      li.classList.add("plus");
-      li.innerHTML = `${data.TextValue}<span>${data.Diposit}</span><span>$${data.AmountValue}</span>`;
-      List.appendChild(li);
-    });
+    return;
   }
+  datas.push({ TextValue, AmountValue, type: "Diposit" });
+
+  let DepositValue = Deposit.innerText;
+  let BalanceValue = Balance.innerText;
+
+  let newDepositValue = parseInt(DepositValue) + parseInt(AmountValue);
+  let newBalanceValue = parseInt(BalanceValue) + parseInt(AmountValue);
+
+  Deposit.innerText = newDepositValue;
+  Balance.innerText = newBalanceValue;
+  updateUI();
 }
 function Withdraw() {
   event.preventDefault();
   let TextValue = Text.value;
   let AmountValue = Amount.value;
+
   if (parseInt(AmountValue) < 100) {
     alert("Minimum Withdraw value is $100");
-  } else {
-    datas.push({ TextValue, AmountValue, Withdraw: "Withdraw" });
-    let WithdrawValue = Withdrow.innerText;
-    let BalanceValue = Balance.innerText;
-
-    let newWithdrawValue = parseInt(WithdrawValue) + parseInt(AmountValue);
-    let newBalanceValue = parseInt(BalanceValue) - parseInt(AmountValue);
-
-    Balance.innerText = newBalanceValue;
-    Withdrow.innerText = newWithdrawValue;
-
-    List.innerHTML = "";
-    datas.map((data) => {
-      let li = document.createElement("li");
-      li.classList.add("minus");
-      li.innerHTML = `${data.TextValue} <span>${data.Withdraw}</span> <span>$${data.AmountValue}</span>`;
-      List.appendChild(li);
-    });
+    return;
   }
+
+  let currentBalance = parseInt(Balance.innerText);
+  if (parseInt(AmountValue) > currentBalance) {
+    alert("Your account do not have balance");
+    return;
+  }
+  datas.push({ TextValue, AmountValue, type: "Withdraw" });
+
+  let WithdrawValue = Withdrow.innerText;
+  let BalanceValue = Balance.innerText;
+
+  let newWithdrawValue = parseInt(WithdrawValue) + parseInt(AmountValue);
+  let newBalanceValue = parseInt(BalanceValue) - parseInt(AmountValue);
+
+  Balance.innerText = newBalanceValue;
+  Withdrow.innerText = newWithdrawValue;
+
+  updateUI();
 }
